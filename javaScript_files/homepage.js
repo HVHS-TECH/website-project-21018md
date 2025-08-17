c1565806aabb2e742d1decb99b2cc8ab61a9722f
Mark Dao <21018md@hvhs.school.nz>
2025-08-18 00:14:55 +1200

    media query breakpoint at 425px

--- javaScript_files/homepage.js
+++ javaScript_files/homepage.js
@@ -4,3 +4,52 @@
 console.log("hompage.js");
 
 /*****************************************************/
+
+function english () {
+    console.log("change to english");
+
+    document.getElementById("titleTop").textContent = "ENTER THE";
+    document.getElementById("titleBottom").textContent = "PREHISTORIC WORLD";
+    document.getElementById("openingInfo").textContent = "Travel back to a time long before humans, filled with bizarre creatures and vast landscapes - welcome to the age of dinosaurs!";
+
+    document.getElementById("dinoHead").textContent = "THE TITANS";
+
+    document.getElementById("dinoDescription").textContent = "During the Mesozoic Era, over 250 million years ago, Earth was dominated by a group of reptiles known as dinosaurs. From colossal claws to tiny teeth, these creatures ran amok throughout the Triassic, Jurassic, and Cretaceous periods. After the asteroid caused their extinction approximately 66 million years ago, scientific research has let us slowly rediscover their world.";
+
+    document.getElementById("dinoSelectButton").textContent = "Dinosaurs -->";
+
+    document.getElementById("languages").textContent = "English";
+}
+
+function japanese () {
+    console.log("change to japanese");
+
+    document.getElementById("titleTop").textContent = "先史時代の世界へ";
+    document.getElementById("titleBottom").textContent = "ようこそ";
+    document.getElementById("openingInfo").textContent = "人類誕生以前の奇妙な生き物や広大な風景に満ちた時代へ旅立ちましょう。恐竜の時代へようこそ!";
+
+    document.getElementById("dinoHead").textContent = "THE TITANS";
+
+    document.getElementById("dinoDescription").textContent = "During the Mesozoic Era, over 250 million years ago, Earth was dominated by a group of reptiles known as dinosaurs. From colossal claws to tiny teeth, these creatures ran amok throughout the Triassic, Jurassic, and Cretaceous periods. After the asteroid caused their extinction approximately 66 million years ago, scientific research has let us slowly rediscover their world.";
+
+    document.getElementById("dinoSelectButton").textContent = "Dinosaurs -->";
+
+    document.getElementById("languages").textContent = "日本語";
+}
+
+function vietnamese () {
+    console.log("change to vietnamese");
+
+    document.getElementById("titleTop").textContent = "NHẬP VÀO";
+    document.getElementById("titleBottom").textContent = "THẾ GIỚI TIỀN SỬ";
+    document.getElementById("openingInfo").textContent = "Quay ngược thời gian trở về thời kỳ rất lâu trước khi con người xuất hiện, nơi có nhiều sinh vật kỳ lạ và cảnh quan rộng lớn - chào mừng đến với thời đại khủng long!";
+
+    document.getElementById("dinoHead").textContent = "CON TITAN";
+
+    document.getElementById("dinoDescription").textContent = "Trong Kỷ Trung Sinh, hơn 250 triệu năm trước, Trái Đất bị thống trị bởi một nhóm bò sát được gọi là khủng long. Từ những móng vuốt khổng lồ đến những chiếc răng nhỏ xíu, những sinh vật này đã hoành hành khắp các kỷ Trias, Jura và Phấn Trắng. Sau khi một tiểu hành tinh gây ra sự tuyệt chủng của chúng khoảng 66 triệu năm trước, nghiên cứu khoa học đã cho phép chúng ta dần khám phá lại thế giới của chúng.";
+
+    document.getElementById("dinoSelectButton").textContent = "Khủng long -->";
+
+    document.getElementById("languages").textContent = "Tiếng việt";
+}
+
